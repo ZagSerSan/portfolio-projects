a9749a713615d7e227885316879d645ce8c90ccb
ZagSerSan <yan.steys1997@gmail.com>
2023-08-30 11:04:01 +0200

    upd

--- app/weather-widget/src/services/geo.service.jsx
+++ app/weather-widget/src/services/geo.service.jsx
@@ -3,7 +3,7 @@ import config from '../config.json'
 
 export const getGeolocation = async (sityName) => {
   const API_key = config.API_key
-  const link = `http://api.openweathermap.org/geo/1.0/direct?q=${sityName}&limit=1&appid=${API_key}`
+  const link = `https://api.openweathermap.org/geo/1.0/direct?q=${sityName}&limit=1&appid=${API_key}`
 
   const {data} = await axois.get(link)
   return data[0]
